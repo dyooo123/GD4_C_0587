a24312f226fe15606419187b03bf397694c1704e
aldyo21 <aldyoputra21@gmail.com>
2022-09-22 22:16:34 +0700

    UGD4_C_0587

--- process/registerProcess.php
+++ process/registerProcess.php
@@ -1,52 +1,53 @@
-<?php
-    // untuk ngecek tombol yang namenya 'register' sudah di pencet atau belum
-    // $_POST itu method di formnya
+<?php 
+    //Untuk ngecek tombol yang namenya 'register' sudah dipencet atau belum 
+    // $_POST itu method diformnya
+
     if(isset($_POST['register'])){
-    
-        // untuk mengoneksikan dengan database dengan memanggil file db.php
+
+        //Untuk mengoneksikan dengan database dengan memeangill file db.php
         include('../db.php');
-    
-        // tampung nilai yang ada di from ke variabel
-        // sesuaikan variabel name yang ada di registerPage.php disetiap input
+
+        //Untuk menampung nilai yang ada diform kevariabel
+        //Menyesuaikan variabel name yang ada diregister.php disetiap input
         $email = $_POST['email'];
         $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
         $name = $_POST['name'];
         $phonenum = $_POST['phonenum'];
         $membership = $_POST['membership'];
-        // Melakukan insert ke databse dengan query dibawah ini
-        $query = mysqli_query($con,
-            "INSERT INTO users(email, password, name, phonenum, membership)
-        VALUES
-        ('$email', '$password', '$name', '$phonenum', '$membership')")
-    or die(mysqli_error($con)); // perintah mysql yang gagal dijalankan ditangani oleh perintah “or die”
-    $isEmailAlready = "SELECT email from users WHERE email='$email'";
-    $checkEmail = mysqli_query($con, $isEmailAlready);
+        $cekEmail = mysqli_query($con, "SELECT * FROM users WHERE email = '$email'")
+or die(mysqli_error($con));
+        //Melakukan insert ke database dengan query dibawah ini
+        if(mysqli_num_rows($cekEmail) == 0){
+            $query = mysqli_query($con,
+            "INSERT INTO users(email,password,name,phonenum,membership)
+                VALUES
+                    ('$email', '$password', '$name', '$phonenum', '$membership')")
+                        or die(mysqli_error($con)); 
 
-    if($checkEmail){
+            if($query){
+                echo
+                    '<script>
+                    alert("Register Success");
+                    window.location = "../index.php"
+                    </script>';
+            }else{
+                echo
+                    '<script>
+                    alert("Register Failed");
+                    </script>';
+            }
+        }else{
+            echo
+            '<script>
+                alert("Email is already Taken!, Failed to Register!");
+                window.location = "../page/registerPage.php";
+            </script>';
+        }
+
+    }else{
         echo
         '<script>
-        alert("Email is already taken, Failed to Register!");
-        window.location = "../index.php"
+        window.history.back()
         </script>';
-
-        return;
     }
-    if($query){
-        echo
-            '<script>
-            alert("Register Success");
-            window.location = "../index.php"
-            </script>';
-    }else{
-        echo
-            '<script>
-            alert("Register Failed");
-            </script>';
-    }
-    }else{
-        echo
-            '<script>
-            window.history.back()
-            </script>';
-            }
 ?>
